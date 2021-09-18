b1463f9608330dca71551451257f82d04249fbdf
19537 <1953745128@qq.com>
2021-09-18 17:37:23 +0800

    数组的默认初始化值

---
+++ Array/src/d0916/ArrayTest1.java
@@ -0,0 +1,33 @@
+package d0916;
+
+public class ArrayTest1 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[1];
+        System.out.println(arr[0]);
+
+        short[] arr2 = new short[1];
+        System.out.println(arr2[0]);
+
+        float[] arr3 = new float[1];
+        System.out.println(arr3[0]);
+
+        double[] arr4 = new double[1];
+        System.out.println(arr4[0]);
+
+        char[] arr5 = new char[1];
+        System.out.println(arr5[0]);
+
+        boolean[] arr7 =new boolean[1];
+        System.out.println(arr7[0]);
+
+        Integer[] arr1 = new Integer[1];
+        System.out.println(arr1[0]);
+
+        String[] arr6 = new String[1];
+        System.out.println(arr6[0]);
+
+        Boolean[] arr8 =new Boolean[1];
+        System.out.println(arr8[0]);
+    }
+}
