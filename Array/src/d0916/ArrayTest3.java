045e62375303f2ea75dea7d1449391d035840352
19537 <1953745128@qq.com>
2021-09-18 22:09:10 +0800

    二维数组的初始化值

---
+++ Array/src/d0916/ArrayTest3.java
@@ -0,0 +1,9 @@
+package d0916;
+
+public class ArrayTest3 {
+    public static void main(String[] args) {
+        int[][] arr = new int[4][3];
+        System.out.println(arr[0]);
+        System.out.println(arr[0][0]);
+    }
+}
