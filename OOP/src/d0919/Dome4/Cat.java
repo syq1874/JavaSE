ae76432dc6011ec839cbdb90d505c94cba3891d5
19537 <1953745128@qq.com>
2021-09-19 15:47:58 +0800

    this关键字

---
+++ OOP/src/d0919/Dome4/Cat.java
@@ -0,0 +1,32 @@
+package d0919.Dome4;
+
+public class Cat {
+    String name;
+
+    //无参构造函数
+    public Cat() {
+
+    }
+
+    //有参构造函数
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void eat() {
+        System.out.print(name);
+        System.out.println("吃猫粮");
+    }
+
+    public void print() {
+        //调用成员方法
+        this.eat();
+    }
+
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.print();
+        Cat c2 =new Cat("小花");
+        c2.print();
+    }
+}
