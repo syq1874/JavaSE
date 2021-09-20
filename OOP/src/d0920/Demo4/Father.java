f4ba6c709c21265fe1ca6f5833d56b9ec10c6325
19537 <1953745128@qq.com>
2021-09-20 20:20:49 +0800

    父类和子类方法不重名

---
+++ OOP/src/d0920/Demo4/Father.java
@@ -0,0 +1,7 @@
+package d0920.Demo4;
+
+public class Father {
+    public void show(){
+        System.out.println("父类中的show方法执行");
+    }
+}
---
+++ OOP/src/d0920/Demo4/Test.java
@@ -0,0 +1,9 @@
+package d0920.Demo4;
+
+public class Test {
+    public static void main(String[] args) {
+        Son son = new Son();
+        son.show();
+        son.show2();
+    }
+}
