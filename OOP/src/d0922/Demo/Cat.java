a48284378f9837822bacffcbdf14d0c9750a8096
19537 <1953745128@qq.com>
2021-09-22 13:55:01 +0800

    多态的使用

---
+++ OOP/src/d0922/Demo/Cat.java
@@ -0,0 +1,9 @@
+package d0922.Demo;
+
+public class Cat extends Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("吃鱼!");
+    }
+}
---
+++ OOP/src/d0922/Demo/Test.java
@@ -0,0 +1,14 @@
+package d0922.Demo;
+
+public class Test {
+    public static void main(String[] args) {
+        Cat cat =new Cat();
+        cat.eat();
+        Dog dog =new Dog();
+        dog.eat();
+        Animal cat2 =new Cat();
+        cat2.eat();
+        Animal dog2 =new Dog();
+        dog2.eat();
+    }
+}
