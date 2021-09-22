95526d33f251b9edac9728a50cca229f82a38d11
19537 <1953745128@qq.com>
2021-09-22 14:54:15 +0800

    向下转型

---
+++ OOP/src/d0922/Demo5/Animal.java
@@ -0,0 +1,7 @@
+package d0922.Demo5;
+
+public class Animal {
+    public void eat(){
+        System.out.println("吃东西！");
+    }
+}
---
+++ OOP/src/d0922/Demo5/Cat.java
@@ -0,0 +1,13 @@
+package d0922.Demo5;
+
+public class Cat extends Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("吃鱼!");
+    }
+
+    public void catchMouse() {
+        System.out.println("抓老鼠!");
+    }
+}
