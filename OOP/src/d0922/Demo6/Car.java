aeb019d88d0e86f5e674b1d192aa3a6ab747ad7a
19537 <1953745128@qq.com>
2021-09-22 15:01:32 +0800

    toString方法

---
+++ OOP/src/d0922/Demo6/Car.java
@@ -0,0 +1,14 @@
+package d0922.Demo6;
+
+public class Car {
+    String name;
+    double price;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ OOP/src/d0922/Demo6/Test.java
@@ -0,0 +1,8 @@
+package d0922.Demo6;
+
+public class Test {
+    public static void main(String[] args) {
+        Car car =new Car();
+        System.out.println(car.toString());
+    }
+}
